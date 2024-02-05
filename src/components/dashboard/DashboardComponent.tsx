5d26fa501aabfe01640758e9a8f0a0fa8433cf1e
Diego Choli <diegocholi@gmail.com>
2024-02-05 15:38:04 -0300

    Remove deps

--- src/components/dashboard/DashboardComponent.tsx
+++ src/components/dashboard/DashboardComponent.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect, PropsWithChildren, FormEvent } from 'react'
+import { useState, useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
 import Card from '@/components/card'
 import PieChart from '../charts/PieChart'
